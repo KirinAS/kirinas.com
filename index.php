05576be34cd31d17efe3991220cc04989a2dc024
Felix Wong <felix@fawong.com>
2014-01-03 23:39:26 -0800

    updated index with production info

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 <div class="page-header">
 <h1>Kirin Art School</h1>
 </div>
+
+<p>Producers:	Bae Yong-joon, Park Jin-young</p>
+<p>Location: Seoul, South Korea</p>
+<p>Running time: Mondays and Tuesdays at 21:55 (9:55 PM) KST</p>
+<p>Production companys: KeyEast Entertainment, JYP Entertainment, CJ Media</p>
+<p>Distributor:	Korean Broadcasting System (KBS)</p>
+
 </div>
 
 <?php include_once("footer.php"); ?>
