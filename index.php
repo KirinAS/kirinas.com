903e528d57bd005837fbcb1d8ae8056a443f331e
Felix Wong <felix@fawong.com>
2014-01-10 12:37:16 +0100

    more production info

--- index.php
+++ index.php
@@ -6,11 +6,30 @@
 <h1>Kirin Art School</h1>
 </div>
 
+<p><strong>Genre:</strong> Comedy Romance Music</p>
+<p><strong>Format:</strong> Television series</p>
+<p><strong>Written by:</strong> Park Hye-ryun</p>
+<p><strong>Directed by:</strong> Lee Eung-bok</p>
+<p><strong>Starring:</strong> Bae Suzy, Kim Soo-hyun, Ok Taecyeon, Ham Eunjung, Jang Wooyoung, IU</p>
+<p><strong>Opening theme:</strong> Dream High</p>
+<p><strong>Ending theme:</strong> Only Hope</p>
+<p><strong>Country of origin:</strong> South Korea
+<p><strong>Original language:</strong> Korean
+<p><strong>Number of seasons:</strong> 1</p>
+<p><strong>Number of episodes:</strong> 16 (1 special)</p>
 <p><strong>Producers:</strong> Bae Yong-joon, Park Jin-young</p>
 <p><strong>Location:</strong> Seoul, South Korea</p>
 <p><strong>Running time:</strong> Mondays and Tuesdays at 21:55 (9:55 PM) KST</p>
 <p><strong>Production companies:</strong> KeyEast Entertainment, JYP Entertainment, CJ Media</p>
 <p><strong>Distributor:</strong> Korean Broadcasting System (KBS)</p>
+<p><strong>Original broadcast channel:</strong> KBS2</p>
+<p><strong>Picture format:</strong> 480i SDTV, 1080i HDTV</p>
+<p><strong>Audio format:</strong> Dolby Digital, DTS Digital Sound, Sony Dynamic Digital Sound</p>
+<p><strong>Original run:</strong> 3 January 2011 &mdash; 28 February 2011</p>
+<p><strong>Preceded by:</strong> Marry Me, Mary!</p>
+<p><strong>Followed by:</strong> Detectives in Trouble</p>
+<p><strong>Related show:</strong> Dream High 2</p>
+*information courtesy <a href="//en.wikipedia.org/wiki/Dream_High">Wikipedia</a>
 
 </div>
 
