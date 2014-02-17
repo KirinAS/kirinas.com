ca436d21e46803b576d0cc50eb57c15b2600e84e
Felix Wong <felix@fawong.com>
2014-02-17 03:32:06 -0800

    update metadata

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="Kirin Art School Homepage">
-<meta name="author" content="">
+<meta name="description" content="Kirin Art School">
+<meta name="author" content="Kirin Art School">
 <link rel="shortcut icon" href="/favicon.ico">
 
 <?php
