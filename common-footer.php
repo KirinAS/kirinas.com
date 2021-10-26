e0f98512f99f3b35cd812f659f150e086335c319
Felix Wong <felix@waf.hk>
2021-10-26 02:33:54 +0000

    update submodules

--- common-footer.php
+++ common-footer.php
@@ -1,4 +1,8 @@
 <?php
+$footers = [
+    "Respective trademarks/copyright belong to KBS, KBS America, KeyEast Entertainment, JYP Entertainment, and CJ Media. This is a fan made site.",
+    "Source code located at <a href=\"https://github.com/KirinAS/kirinas.com\" target=\"_blank\">GitHub</a>",
+];
 
 include_once("common/footer.php");
 ?>
--- common-header.php
+++ common-header.php
@@ -3,11 +3,11 @@ $title = "Kirin Art School";
 
 $links = [
   "/" => "Home",
-  "/goham" => "Go Hye Mi",
-  "/ssd" => "Song Sam Dong",
-  "/jin-k" => "Jin Guk/Hyun Shi Hyuk",
-  "/ybh" => "Yoon Baek Hee",
-  "/IU" => "Kim Pil Sook",
+  "goham" => "Go Hye Mi",
+  "ssd" => "Song Sam Dong",
+  "jin-k" => "Jin Guk/Hyun Shi Hyuk",
+  "ybh" => "Yoon Baek Hee",
+  "IU" => "Kim Pil Sook",
 ];
 
 include_once("common/header.php");
